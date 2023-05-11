40f9b70fc4a3fb65148da9a312c35a1b7fb7a562
Evgeny Kalashnikov <ekalashnikov@gmail.com>
2023-05-11 12:28:39 -0400

    installed laravel orion for api + updated products table migration to include price

---
+++ app/Models/Products.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+class Products
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+        'image',
+    ];
+
+}
